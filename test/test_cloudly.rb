f4da6d43b0b31a0f1eb2cf3e75dea4cd192299d1
Wade West <wwest81@gmail.com>
2010-10-12 23:22:02 -0500

    Testing getting a list of items from CloudApp.

--- test/test_cloudly.rb
+++ test/test_cloudly.rb
@@ -1,10 +1,22 @@
 require 'helper'
 
 class TestCloudAppAPI < Test::Unit::TestCase
-  should "Test for username and password for CloudApp" do
+  
+  should "Test for username and password for CloudApp." do
     email = cloudapp_config['email']
     password = cloudapp_config['password']
     flunk "The test will need your email" if email.blank?
     flunk "The test will need your password" if password.blank?
   end
+  
+  should "Test retreiving a listing of my uploaded files." do
+    client = CloudApp::Client.new :username => cloudapp_config['email'],
+                                  :password => cloudapp_config['password']
+    flunk "Couldn't create client" if client.nil?
+    items = client.items
+    unless items.code == 200  # something is wrong with HTTParty::Response#ok?
+      flunk "Couldn't login to CloudApp.  Check your email and password in test_config.yml"
+    end
+  end
+  
 end
