47fd8eb2e223863bdc05291300ce0258763381dc
Wade West <wwest81@gmail.com>
2010-10-12 22:11:09 -0500

    Not sure what "cloudly" is, it's not a gem.  But it seems I don't need it.

--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'cloudly'
+#require 'cloudly'
 
 class Test::Unit::TestCase
 end
