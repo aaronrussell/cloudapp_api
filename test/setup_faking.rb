98094974da32786e61003d4eb23eb08687617414
Wade West <wwest81@gmail.com>
2010-10-15 11:57:10 -0500

    Some more WebFaker setup

--- test/setup_faking.rb
+++ test/setup_faking.rb
@@ -4,7 +4,8 @@ def stub_file( file )
   File.join(TEST_DIR, 'fakeweb', file)
 end
 
-#FakeWeb.allow_net_connect = false
+FakeWeb.allow_net_connect = false
 
-FakeWeb.register_uri :head, "http://my.cl.ly/items", :response => stub_file('listing.head')
-FakeWeb.register_uri :get, "http://my.cl.ly/items",  :response => stub_file('listing.response')
+FakeWeb.register_uri :head, %r|^http://my.cl.ly/items|, :response => stub_file('auth.head')
+FakeWeb.register_uri :get,  "http://my.cl.ly/items",    :response => stub_file('listing.response')
+FakeWeb.register_uri :post, "http://my.cl.ly/items",    :response => stub_file('create_bookmark.response')
