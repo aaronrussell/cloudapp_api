bef0d420e49628b979da1f5092a2c75f75f2ae65
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-12-15 17:41:15 +0100

    Improve documentation of Client

--- lib/cloudapp/client.rb
+++ lib/cloudapp/client.rb
@@ -31,9 +31,12 @@ module CloudApp
   class Client
     
     # Creates a new CloudApp::Client instance.
-    # @example Optionally provide an authentication hash
-    #   { :username => "testuser@test.com", :password => "password" }
-    # @param [Hash] authentication credentials.
+    #
+    # You can pass +:username+ and +:password+ parameters to the call.
+    #
+    # @param [Hash] opts authentication credentials.
+    # @option opts [String] :username cl.ly username
+    # @option opts [String] :password cl.ly username
     # @return [CloudApp::Client]
     def initialize(opts = {})
       if opts[:username] && opts[:password]
@@ -42,8 +45,9 @@ module CloudApp
     end
     
     # Sets the authentication credentials in a class variable.
-    # @param [String] cl.ly username
-    # @param [String] cl.ly password
+    #
+    # @param [String] username cl.ly username
+    # @param [String] password cl.ly password
     # @return [Hash] authentication credentials
     def authenticate(username, password)
       Base.authenticate(username, password)
@@ -51,29 +55,33 @@ module CloudApp
     
     # Get metadata about a cl.ly URL like name, type, or view count.
     # Finds the item by it's slug id, for example "2wr4".
-    # @param [String] cl.ly slug id
+    #
+    # @param [String] id cl.ly slug id
     # @return [CloudApp::Item]
     def item(id)
       Item.find(id)
     end
     
     # Page through your items.
-    # @example Optionally pass a hash of parameters.
-    #   :page => 1        # Page number starting at 1
-    #   :per_page => 5    # Number of items per page
-    #   :type => image    # Filter items by type (image, bookmark, text, archive, audio, video, or unknown)
-    #   :deleted => true  # Show trashed items
+    #
     # Requires authentication.
-    # @param [Hash] options parameters
-    # @return [Array] of CloudApp::Item items
+    #
+    # @param [Hash] opts options parameters
+    # @option opts [Integer] :page (1) Page number starting at 1
+    # @option opts [Integer] :per_page (5) Number of items per page
+    # @option opts [String] :type ('image') Filter items by type (image, bookmark, text, archive, audio, video, or unknown)
+    # @option opts [Boolean] :deleted (true) Show trashed items
+    # @return [Array[CloudApp::Item]]
     def items(opts = {})
       Item.all(opts)
     end
     
     # Create a new cl.ly item by bookmarking a link.
+    #
     # Requires authentication.
-    # @param [String] url to bookmark
-    # @param [String] name of bookmark
+    #
+    # @param [String] url url to bookmark
+    # @param [String] name name of bookmark
     # @return [CloudApp::Item]
     def bookmark(url, name = "")
       Item.create(:bookmark, {:name => name, :redirect_url => url})
@@ -81,7 +89,7 @@ module CloudApp
     
     # Create a new cl.ly item by uploading a file.
     # Requires authentication.
-    # @param [String] local path to file
+    # @param [String] file local path to file
     # @return [CloudApp::Item]
     def upload(file)
       Item.create(:upload, :file => file)
@@ -90,8 +98,8 @@ module CloudApp
     # Change the name of an item.
     # Finds the item by it's slug id, for example "2wr4".
     # Requires authentication.
-    # @param [String] cl.ly item id
-    # @param [String] new item name
+    # @param [String] id cl.ly item id
+    # @param [String] name new item name
     # @return [CloudApp::Item]
     def rename(id, name = "")
       item = Item.find(id)
@@ -101,8 +109,8 @@ module CloudApp
     # Modify an item with a private URL to have a public URL or vice versa.
     # Finds the item by it's slug id, for example "2wr4".
     # Requires authentication.
-    # @param [String] cl.ly item id
-    # @param [Boolean] privacy setting
+    # @param [String] id cl.ly item id
+    # @param [Boolean] privacy privacy setting
     # @return [CloudApp::Item]
     def privacy(id, privacy = false)
       item = Item.find(id)
@@ -111,8 +119,10 @@ module CloudApp
     
     # Send an item to the trash.
     # Finds the item by it's slug id, for example "2wr4".
+    #
     # Requires authentication.
-    # @param [String] cl.ly item id
+    #
+    # @param [String] id cl.ly item id
     # @return [CloudApp::Item]
     def delete(id)
       item = Item.find(id)
