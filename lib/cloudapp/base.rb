734e885b7d3399b2dc21c0311e34808112c8ced5
cn <cn@mycrobase.de>
2013-03-30 14:12:38 +0100

    Parse date/times automatically on behalf of Crack
    HTTParty 0.8.0 and above don't use Crack (and it's very special feature of automatically parsing dates and times from JSON)
    as default engine anymore and the JSON standard does not include a Date type anyways so I move some explicit parsing code
    into Base.load.
    
    Reference: https://github.com/jnunemaker/httparty/issues/100

--- lib/cloudapp/base.rb
+++ lib/cloudapp/base.rb
@@ -59,7 +59,19 @@ module CloudApp
     # @param [Hash] attributes
     def load(attributes = {})
       attributes.each do |key, val|
-        self.instance_variable_set("@#{key}", val)
+        if key =~ /^.*_at$/ and val
+          # if this is a date/time key and it's not nil, try to parse it first
+          # as DateTime, then as Date only
+          begin
+            dt = DateTime.strptime(val, "%Y-%m-%dT%H:%M:%SZ")
+            newval = Time.utc(dt.year, dt.month, dt.day, dt.hour, dt.min, dt.sec)
+          rescue ArgumentError => ex
+            newval = DateTime.strptime(val, "%Y-%m-%d")
+          end
+          self.instance_variable_set("@#{key}", newval)
+        else
+          self.instance_variable_set("@#{key}", val)
+        end
       end
     end
     
